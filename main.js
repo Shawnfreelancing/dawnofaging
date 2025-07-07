41b586ec2b26e691b54b8ce545902f43b0e8b272
Shawnfreelancing <shawnfr33lance@gmail.com>
2025-07-06 21:26:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,4 +5,9 @@ hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('open');
 });
 
-  
+    document.querySelectorAll('.accordion-header').forEach(header => {
+      header.addEventListener('click', () => {
+        const accordion = header.parentElement;
+        accordion.classList.toggle('open');
+      });
+    });
